df57d6b33ef8e02c6f5736964539f126561943c5
yoko275 <kiyomi.yokomizo@gmail.com>
2019-07-26 16:34:09 +0900

    test1

--- app/controllers/itdict_bot_controller.rb
+++ app/controllers/itdict_bot_controller.rb
@@ -3,18 +3,27 @@ class ItdictBotController < ApplicationController
     # callbackアクションのCSRFトークン認証を無効
     protect_from_forgery :except => [:callback]
     
-    def callback
-        body = request.body.read
-        signature = request.env['HTTP_X_LINE_SIGNATURE']
-        unless client.validate_signature(body, signature)
-        error 400 do 'Bad Request' end
+    def client
+        @client ||= Line::Bot::Client.new { |config|
+        config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+        config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+        }
     end
     
-    events = client.parse_events_from(body)
-    events.each { |event|
-    case event
-        when Line::Bot::Event::Message
-            case event.type
+    def callback
+        body = request.body.read
+
+        signature = request.env['HTTP_X_LINE_SIGNATURE']
+        unless client.validate_signature(body, signature)
+            head :bad_request 
+        end
+    
+        events = client.parse_events_from(body)
+        
+        events.each { |event|
+            case event
+            when Line::Bot::Event::Message
+                case event.type
                 when Line::Bot::Event::MessageType::Text
                 #    msg = event.message['text']
                 #    id = msg.to_s
@@ -33,16 +42,11 @@ class ItdictBotController < ApplicationController
                     client.reply_message(event['replyToken'], message)
                 end
             end
-            }
-            head :ok
-        end
-    private
-    def client
-        @client ||= Line::Bot::Client.new { |config|
-        config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-        config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
         }
+        
+        head :ok
     end
+    #private
     
     def find_dict(ew)
         dict = Dict.find_by(eword: ew)
