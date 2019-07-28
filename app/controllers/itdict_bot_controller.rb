d546a45edc20ae0b30317101137c36908f975e06
yoko275 <kiyomi.yokomizo@gmail.com>
2019-07-28 11:38:52 +0900

    try again

--- app/controllers/itdict_bot_controller.rb
+++ app/controllers/itdict_bot_controller.rb
@@ -1,37 +1,60 @@
-class LinebotController < ApplicationController
-    require 'line/bot'
+class ItdictBotController < ApplicationController
+  require 'line/bot'
+  # callbackアクションのCSRFトークン認証を無効
+  protect_from_forgery :except => [:callback]
   
-    def callback
+  def callback
       body = request.body.read
+
       signature = request.env['HTTP_X_LINE_SIGNATURE']
       unless client.validate_signature(body, signature)
-        error 400 do 'Bad Request' end
+          head :bad_request 
       end
+  
       events = client.parse_events_from(body)
-  
-      events.each do |event|
-        case event
-        when Line::Bot::Event::Message
-          case event.type
-          when Line::Bot::Event::MessageType::Text
-            message = {
-              type: 'text',
-              text: event.message['text']
-            }
+      
+      events.each { |event|
+          case event
+          when Line::Bot::Event::Message
+              case event.type
+              when Line::Bot::Event::MessageType::Text
+              #    msg = event.message['text']
+              #    id = msg.to_s
+              #    if find_dict(id)
+              #        dict = find_dict(id)
+              #        message = [{
+              #        type: 'text',
+              #        text: "「#{dict.eword}」\n#{dict.jword}"
+              #        }]
+              #    else
+                      message = {
+                          type: 'text',
+                          text: "該当なし"
+                      }
+              #    end
+                  client.reply_message(event['replyToken'], message)
+              end
           end
-        end
-        client.reply_message(event['replyToken'], message)
-      end
-      head :ok
-    end
-  
-  private
-  
-  # LINE Developers登録完了後に作成される環境変数の認証
-    def client
-      @client ||= Line::Bot::Client.new { |config|
-        config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-        config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
       }
-    end
+      head :ok
   end
+
+  def find_dict(ew)
+      dict = Dict.find_by(eword: ew)
+      if dict
+          return dict
+      else
+          nil
+      end
+  end
+
+  private
+  # LINE Bot環境変数
+  def client
+      @client ||= Line::Bot::Client.new { |config|
+          config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+          config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      }
+  end    
+
+end
